b0a97d1fd11ed033c94044280aee98448f9b790b
OcularCreo <122617813+OcularCreo@users.noreply.github.com>
2023-02-20 14:52:04 -0500

    figuring out some controls

--- app.js
+++ app.js
@@ -2,15 +2,38 @@ const express   = require('express');         //require is similar to include in
 const app       = express(); 
 const http      = require('http');
 const server    = http.createServer(app); 
+const io        = require('socket.io')(server);
 
 const LISTEN_PORT = 8080;
+
+server.listen(LISTEN_PORT);
 const ABS_STATIC_PATH = __dirname + '/public';
+app.use(express.static(ABS_STATIC_PATH));
+
+console.log("Listening to port: " + LISTEN_PORT);
 
 //set up our routes
 app.get('/', function(req, res){    //request, response
     res.sendfile('index.html', {root:ABS_STATIC_PATH});  //require an absolute path
 });
 
-server.listen(LISTEN_PORT);
-app.use(express.static(ABS_STATIC_PATH));
-console.log("Listening to port: " + LISTEN_PORT);
+app.get('/wack', function(req, res){    //request, response
+    res.sendfile('wack.html', {root:ABS_STATIC_PATH});  //require an absolute path
+});
+
+
+//socket.io / websockets stuff
+io.on('connection', (socket)=>{
+    console.log(socket.id + " connected");
+
+    socket.on("disconnected", (data)=>{
+        console.log(socket.id + 'disconnected');
+    });
+
+    //adding custom events
+    socket.on('btnClick', (data)=>{
+        io.sockets.emit("posChange", {x:2, y:0.5, z:0});
+        console.log("changing position");
+    });
+
+});
---
+++ public/js/pickup.js
@@ -0,0 +1,36 @@
+let grab = false; 
+let holding = false;
+let testCube = 0;
+
+let grabbedItem = 0;
+
+let camEl = 0;
+
+window.onload = function(){
+
+    camEl = document.getElementById('camera');
+
+    holding = false;    //determines if object is being held
+    
+    //temporary and used for tests
+    btn = document.getElementById('btn');
+    btn.addEventListener('click', function(){
+        camEl.removeChild(camEl.lastChild);
+    });
+
+}
+
+//makes object with holdable attribute holdable
+AFRAME.registerComponent('holdable', {
+    init:function(){
+        this.el.addEventListener('click', function(){
+            let item = this.cloneNode(true);
+            camEl.appendChild(item);
+            item.setAttribute("position", {x: -0.8, y:-0.8, z:-1});
+            item.setAttribute("rotation", {x: 0, y:45, z:0});
+            item.setAttribute("scale", {x: 0.75, y:0.75, z:0.75});
+            console.log("picked up");
+            this.remove();
+        });
+    }
+});
